8ce4f32e44a08f88dd23eb75d77d38cd081b82a4
shizukamusicbot1 <sachinheroko25@gmail.com>
2025-05-18 22:21:35 +0530

    Update welcome.py

--- BrandrdXMusic/plugins/tools/welcome.py
+++ BrandrdXMusic/plugins/tools/welcome.py
@@ -91,7 +91,7 @@ async def greet_group(_, member: ChatMemberUpdated):
 ๏ 𝗡𝗔𝗠𝗘 ➠ {user.mention}
 ๏ 𝗜𝗗 ➠ {user.id}
 ๏ 𝐔𝐒𝐄𝐑𝐍𝐀𝐌𝐄 ➠ @{user.username}
-๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ [𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚 📿](https://t.me/BRANDEDKING8)
+๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ [➻❥⏤͟͟͞͞𓆩𝐍 𝐎 𝐁 𝐈 𝐓 𝐀💸⎯ꨄ](https://t.me/NOBITA_XD1)
 ➖➖➖➖➖➖➖➖➖➖➖
 """,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/Sustumm_music_bot?startgroup=true")]])
