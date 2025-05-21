5cf44c50c2e1243aa190febdb71e0008ad1134c5
shizukamusicbot1 <sachinheroko25@gmail.com>
2025-05-21 23:04:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/OGNOBITA12/shizukamusicbot2",
+    "https://github.com/shizukamusicbot1/shizukamusicbot-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
